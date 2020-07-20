85d2a026a9087db5206b0148fe10fd2c69b68c0a
Tom-Bovens <tombovens2003@gmail.com>
2020-07-20 16:13:41 +0200

    Save/load system added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 const ChipChat = require('chipchat');
 const get = require('lodash/get');
 const incrementor = {
+    // This function adds delay to the message to ensure the messages are posted in the right order
     autoDelayTime: 500,
     increment: function (timeToIncrease = 1) {
         this.autoDelayTime += (timeToIncrease * 1000);
@@ -14,7 +15,8 @@ const incrementor = {
     }
 };
 
-function nextVideoinPath(conversation, currentIteration = 0, videoContentDescription, specialMessage) {
+function nextVideoinPath(conversation, currentIteration = 0, videoContentDescription, userId, specialMessage) {
+    // This function automatically constructs and sends a message
     const videoArray = [
         "https://www.youtube.com/embed/px-KEHbUrVo", // Video 1
         "https://www.youtube.com/embed/846DWH8soIw", // Video 2
@@ -31,6 +33,7 @@ function nextVideoinPath(conversation, currentIteration = 0, videoContentDescrip
         "https://www.youtube.com/embed/ZzUP4uxOeh0", // Video 13
         "https://www.youtube.com/embed/iEgnFXG03Yk"  // Video 14
     ]
+    bot.users.update(userId, { meta: { hasToured: currentIteration + 'video' }}) // Update the position of the agent in the quiz
     const messages = [
         {
             text: "The next video will show " + videoContentDescription,
@@ -38,7 +41,7 @@ function nextVideoinPath(conversation, currentIteration = 0, videoContentDescrip
             delay: incrementor.set(3)
         },
     ]
-    if (specialMessage) {
+    if (specialMessage) { // Adds a second message
         messages.push(
             {
                 text: specialMessage,
@@ -47,7 +50,7 @@ function nextVideoinPath(conversation, currentIteration = 0, videoContentDescrip
             }
         )
     }
-    messages.push(
+    messages.push( // Puts the messages in an array to be sent in the conversation
         {
             text: videoArray[(currentIteration)],
             contentType: "text/url",
@@ -71,7 +74,7 @@ const bot = new ChipChat({
     token: process.env.TOKEN
 });
 
-
+// Crashes the code if no token is found
 if (!process.env.TOKEN) {
     throw 'No token found, please define a token with export TOKEN=(Webhook token).'
 }
@@ -81,6 +84,7 @@ if (!process.env.TOKEN) {
 // console.log(`Hello ${botUser.role}`);
 //});
 
+// Logs any error produced to the console
 bot.on('error', console.log);
 
 bot.on('conversation.create', (message) => {
@@ -90,7 +94,8 @@ bot.on('conversation.create', (message) => {
     const conversationId = get(message, 'data.conversation.id');
     bot.conversation(conversationId).then(conversation => {
         bot.users.get(userId).then((user) => {
-            if (get(user, 'meta.hasToured', 'false') === 'true') {
+            const userProgress = get(user, 'meta.hasToured', 'false')
+            if (userProgress === 'true') {
                 console.log("Person has toured before.")
                 conversation.say([
                     {
@@ -116,7 +121,7 @@ bot.on('conversation.create', (message) => {
                         ]
                     }
                 ]);
-            } else {
+            } else if (userProgress === 'false') {
                 console.log("Person hasn't toured")
                 conversation.say([
                     {
@@ -142,6 +147,35 @@ bot.on('conversation.create', (message) => {
                         ]
                     }
                 ]);
+            } else if (userProgress.includes("video") == true) {
+                let progressNumber = parseInt(userProgress)
+                console.log(`User has started but not finished a tour. Their position was ${progressNumber}`)
+                conversation.say([
+                    {
+                        text: `Hello ${userName}! It seems you stopped the tour at video ${progressNumber + 1}.`,
+                        role: 'bot',
+                        delay: incrementor.set(3)
+                    },
+                    {
+                        text:"Would you like to start where you left off or do you want to restart the tour?",
+                        role: "bot",
+                        delay: incrementor.increment(4),
+                        actions: [
+                            {
+                                type: "reply",
+                                text: `Skip to ${progressNumber + 1}`,
+                                payload: `${progressNumber - 1} complete`
+                            },
+                            {
+                                type: "reply",
+                                text: "Restart the tour",
+                                payload: "StartTour"
+                            }
+                        ]
+                    }
+                ]);
+            } else {
+                console.log("Can't find status")
             }
         });
     });
@@ -153,7 +187,8 @@ bot.on('message.create.agent.postback.agent', (message, conversation) => {
     const userId = message.user
     console.log(userId)
     if (message.text === "StartTour") {
-        bot.users.update(userId, { meta: { hasToured: 'false' }}).then((user) => {
+        const userId = message.user
+        bot.users.update(userId, { meta: { hasToured: '0video' }}).then((user) => {
             console.log("User meta hasToured = " + user.meta.hasToured)
             conversation.say([
                 {
@@ -191,6 +226,8 @@ bot.on('message.create.agent.postback.agent', (message, conversation) => {
             ]);
         })
     } else if (message.text === "0complete") {
+        const userId = message.user
+        bot.users.update(userId, { meta: { hasToured: '1video' }})
         conversation.say([
             {
                 text: "Alright, you should know how to accept a conversation now!",
@@ -221,40 +258,65 @@ bot.on('message.create.agent.postback.agent', (message, conversation) => {
             }
         ]);
     } else if (message.text === "1complete") {
-        nextVideoinPath(conversation, 2, "how to edit contact fields in conversations.", "This tour has 14 videos by the way, but it shouldn't take up more than 15 minutes.")
+        const userId = message.user
+        nextVideoinPath(conversation, 2, "how to edit contact fields in conversations.", userId, "This tour has 14 videos by the way, but it shouldn't take up more than 15 minutes.")
     } else if (message.text === "2complete") {
-        nextVideoinPath(conversation, 11, "how to finish a conversation by making a form.", "Also, if you don't have the time to take this tour now, you can always close this conversation and re-do the tour later.")
+        const userId = message.user
+        nextVideoinPath(conversation, 3, "how to finish a conversation by making a form.", userId, "Also, if you don't have the time to take this tour now, you can always close this conversation and re-do the tour later.")
     } else if (message.text === "3complete") {
-        nextVideoinPath(conversation, 4, "how to edit your preferences and personal details in Chatshipper.", "Be sure to visit the preferences tab later to select the options that you like best!")
+        const userId = message.user
+        nextVideoinPath(conversation, 4, "how to edit your preferences and personal details in Chatshipper.", userId, "Be sure to visit the preferences tab later to select the options that you like best!")
     } else if (message.text === "4complete") {
-        nextVideoinPath(conversation, 5, "Video is private.")
+        const userId = message.user
+        nextVideoinPath(conversation, 5, "Video is private.", userId)
     } else if (message.text === "5complete") {
-        nextVideoinPath(conversation, 6, "how to send files, videos or photos via Chatshipper.", "We're halfway! Keep going!")
+        const userId = message.user
+        nextVideoinPath(conversation, 6, "how to send files, videos or photos via Chatshipper.", userId, "We're halfway! Keep going!")
     } else if (message.text === "6complete") {
-        nextVideoinPath(conversation, 7, "how to find a contact or conversation with the search bar.")
+        const userId = message.user
+        nextVideoinPath(conversation, 7, "how to find a contact or conversation with the search bar.", userId)
     } else if (message.text === "7complete") {
-        nextVideoinPath(conversation, 8, "how to forward your conversation to another agent or department.")
+        const userId = message.user
+        nextVideoinPath(conversation, 8, "how to forward your conversation to another agent or department.", userId)
     } else if (message.text === "8complete") {
-        nextVideoinPath(conversation, 9, "how to start a chat with one or more colleagues.", "We hit number 10, just 4 more to go!")
+        const userId = message.user
+        nextVideoinPath(conversation, 9, "how to start a chat with one or more colleagues.", userId, "We hit number 10, just 4 more to go!")
     } else if (message.text === "9complete") {
-        nextVideoinPath(conversation, 10, "how to ask a colleague to join your current conversation with a consumer.", "Did you know that this bot took approximately 16 manhours to be finished?")
+        const userId = message.user
+        nextVideoinPath(conversation, 10, "how to ask a colleague to join your current conversation with a consumer.", userId)
     } else if (message.text === "10complete") {
-        nextVideoinPath(conversation, 11, "how to follow and unfollow conversations.")
+        const userId = message.user
+        nextVideoinPath(conversation, 11, "how to follow and unfollow conversations.", userId)
     } else if (message.text === "11complete") {
-        nextVideoinPath(conversation, 12, "how to make and use pre-made (canned) responses in conversations.")
+        const userId = message.user
+        nextVideoinPath(conversation, 12, "how to make and use pre-made (canned) responses in conversations.", userId)
     } else if (message.text === "12complete") {
-        nextVideoinPath(conversation, 13, "how to give feedback on the forms that your colleagues made.", "This is the last video, we're almost here!")
+        const userId = message.user
+        nextVideoinPath(conversation, 13, "how to give feedback on the forms that your colleagues made.", userId, "This is the last video, we're almost here!")
     }  else if (message.text === "13complete") {
-            bot.users.update(userId, { meta: { hasToured: 'true' }}).then((user) => { })
+        const userId = message.user
+            bot.users.update(userId, { meta: { hasToured: 'true' }})
             conversation.say([
                 {
-                    text: "Alright, that was the tour! Come back any time to retake it!",
+                    text: "Alright, that was the tour! If you ever want to retake the tour, come back and i'll restart the tour for you!",
                     role: 'bot',
                     delay: incrementor.set(3)
                 },
             ]);
         };
-    });
+});
+
+bot.on('notify', (message, conversation) => {
+  if (message.type === 'mention') {
+    conversation.say('Wazzup?');
+  } else if (message.type === 'command' && message.text === '/assign') {
+    // assume responsibility of the conversation
+    conversation.accept();
+  } else if (message.type === 'command' && message.text.startsWith('>')) {
+    conversation.say('Wazzup?', { isBackchannel: true });
+  }
+});
+
 
 // Start Express.js webhook server to start listening
 bot.start();
